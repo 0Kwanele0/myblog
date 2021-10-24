991476d41411e95af2326bab88ab5b45da9def68
kwanele <kwanelegamedze4@gmail.com>
2021-10-24 22:54:47 +0200

    edited content

--- pages/post/index.js
+++ pages/post/index.js
@@ -52,8 +52,8 @@ function index() {
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
+            distribution of letters, as opposed to using Content here, content
+            here, making it look like readable English. Many desktop publishing
             packages and web page editors now use (injected humour and the
             like).
           </p>
@@ -62,8 +62,8 @@ function index() {
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
+            distribution of letters, as opposed to using Content here, content
+            here, making it look like readable English. Many desktop publishing
             packages and web page editors now use (injected humour and the
             like).
           </p>
@@ -72,8 +72,8 @@ function index() {
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
+            distribution of letters, as opposed to using Content here, content
+            here, making it look like readable English. Many desktop publishing
             packages and web page editors now use (injected humour and the
             like).
           </p>
